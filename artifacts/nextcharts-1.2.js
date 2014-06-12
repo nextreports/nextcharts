7aff0d2af139b36aa1165dc89440070fcc151335
Mihai <dpmihai@yahoo.com>
2014-06-12 12:54:01 +0300

    add alarm widget

--- artifacts/nextcharts-1.2.js
+++ artifacts/nextcharts-1.2.js
@@ -4778,12 +4778,19 @@ var getData = function(id, zoom) {
 }
 
 // draw texts: title, description, min, max, value
-var drawText = function(id, title, description, unit, min, max, value, showMinMax, d) {
+var drawText = function(id, title, description, unit, min, max, value, showMinMax, d, shadow) {
   var can = document.getElementById(id);
   var ctx = can.getContext('2d');   
   
   // clear canvas
   ctx.clearRect(0, 0, can.width, can.height);
+  
+  if (shadow) {
+	ctx.shadowColor = "#d1ceb2";
+	ctx.shadowOffsetX = 5; 
+	ctx.shadowOffsetY = 5; 
+	ctx.shadowBlur = 5;
+  }
     
   ctx.fillStyle = "black";
   ctx.font="bold " + d.size/10  + "px Arial";
@@ -4809,11 +4816,15 @@ var drawText = function(id, title, description, unit, min, max, value, showMinMa
   }  
   ctx.fillStyle = "black";
   ctx.font="bold " + d.size/6 + "px Arial";
-  ctx.fillText(value,d.x-ctx.measureText(value).width/2,d.y );    
+  ctx.fillText(value,d.x-ctx.measureText(value).width/2,d.y );   
+  
+  ctx.shadowBlur = 0;
+  ctx.shadowOffsetX = 0; 
+  ctx.shadowOffsetY = 0; 
 }
 
 // fill component with color
-var drawColor = function(id, angle, color, title, d) {
+var drawColor = function(id, angle, color, title, d, shadow) {
   
    var can = document.getElementById(id);
    var ctx = can.getContext('2d');      
@@ -4839,9 +4850,18 @@ var drawColor = function(id, angle, color, title, d) {
    ctx.fill();
 
    // draw title again to be over the fill color
+   if (shadow) {
+		ctx.shadowColor = "#d1ceb2";
+		ctx.shadowOffsetX = 5; 
+		ctx.shadowOffsetY = 5; 
+		ctx.shadowBlur = 5;
+   }
    ctx.fillStyle = "black";  
    ctx.font="bold " + d.size/10 + "px Arial";
    ctx.fillText(title,d.x-ctx.measureText(title).width/2,d.y-d.radix+d.arcWidth/2 + d.size/10);
+   ctx.shadowBlur = 0;
+   ctx.shadowOffsetX = 0; 
+   ctx.shadowOffsetY = 0; 
    
 };
 
@@ -4860,10 +4880,10 @@ var drawArc = function(id, d) {
   ctx.stroke();  
 }
 
-function indicatorP(id, color, title, description, unit, min, max, value, showMinMax, zoom) {	  	
+function indicatorP(id, color, title, description, unit, min, max, value, showMinMax, shadow, zoom) {	  	
 	
     var d = getData(id, zoom);    
-    drawText(id, title, description, unit, min, max, value, showMinMax, d);
+    drawText(id, title, description, unit, min, max, value, showMinMax, d, shadow);
     
     if (value > max) {
         value = max;
@@ -4879,7 +4899,7 @@ function indicatorP(id, color, title, description, unit, min, max, value, showMi
        $({ n: from }).animate({ n: to}, {
           duration: 1000,    
           step: function(now, fx) {
-             drawColor(id, now*Math.PI/180, color, title, d);       
+             drawColor(id, now*Math.PI/180, color, title, d, shadow);       
           } 
        });  
     }
@@ -4941,7 +4961,12 @@ function indicator(id, myjson, zoom) {
 		color = "blue";
 	}	
 	
-	indicatorP(id, color, title, description, unit, min, max, value, showMinMax, zoom); 
+	var shadow = myjson.shadow;
+	if (typeof shadow === "undefined") {
+		shadow = false;
+	}	
+	
+	indicatorP(id, color, title, description, unit, min, max, value, showMinMax, shadow, zoom); 
 	
 }
 
@@ -5093,12 +5118,194 @@ function drawArrow(c, dotX, dotY, up, size, shouldRise) {
 	c.strokeStyle="gray";
 	c.stroke();
 }
+
+/*
+ * Alarm (status)
+ * 
+ * {
+ * 		"text" : "Project is on track.",
+ * 		"color" : "green",
+ * 		"background" : "white",
+ * 		"shadow" : true
+ * }
+ * 
+ */
+function alarm(id, myjson, zoom) {
+	
+	var can = document.getElementById(id);
+	var ctx = can.getContext('2d');  
+	    
+	if (zoom == true) {	  
+	    can.width = $(window).width();
+		can.height = $(window).height();	  
+	}
+	
+	var canWidth = can.width;
+	var canHeight = can.height;		
+	var textSize = canHeight/10;	
+	
+	var text = myjson.text;
+	if (typeof text === "undefined") {
+	    text = "";
+	}
+	
+	var color = myjson.color;
+	if (typeof color === "undefined") {
+		color = "green";
+	}
+	
+	var background = myjson.background;
+	if (typeof background === "undefined") {	
+		background = "white";
+	}
+	
+	var shadow = myjson.shadow;
+	if (typeof shadow === "undefined") {
+		shadow = false;
+	}	
+	
+	if (shadow) {
+		ctx.shadowColor = "#d1ceb2";
+		ctx.shadowOffsetX = 5; 
+		ctx.shadowOffsetY = 5; 
+		ctx.shadowBlur = 5;
+	}
+	
+	var size = canWidth;
+	if (canHeight < canWidth) {
+		size = canHeight;
+	}
+	var left = canWidth/20;        // left padding
+	var x = Math.pow(size,1.1)/8;  // top-bottom padding
+	var d = 2;                     // distance between circle and border
+	var radius = (size - 2 * x) / 2;
+	var fontSize = Math.log(size/12)*9;
+	
+	// clear canvas
+	ctx.clearRect(0, 0, can.width, can.height);
+	ctx.fillStyle = background; 	
+	ctx.fillRect(0, 0, can.width, can.height);	
+	    
+	//text
+	ctx.fillStyle = "black";
+	ctx.font=fontSize  + "px Arial";
+	
+	var xText = 3*left/2+ 2*radius;
+	var yText = canHeight/2+ fontSize/4;
+	var textWidth = ctx.measureText(text).width + left;  
+	var lines = new Array();
+	
+	if (xText + textWidth > canWidth) {				
+		// text fills multiple lines		
+		var res = text.split(" ");
+		var words = res.length;
+		var line = "";
+		for (var k=0; k<words; k++) {				
+			var sline = line + res[k] + " ";
+			var accWidth = ctx.measureText(sline).width + left;
+			if (xText + accWidth > canWidth) {
+				lines.push(line);				
+				line = res[k] + " ";				
+			} else {
+				line = sline;
+			}
+		}		
+		lines.push(line);
+		var linesNo = lines.length;
+		var odd = ((linesNo % 2) != 0);
+		var mid = Math.floor(linesNo/2);		
+		
+		if (odd) {
+			for (var line=0; line<mid; line++) {
+				var y = yText -(mid-line)*fontSize  ;					
+				ctx.fillText(lines[line],xText,y);
+			}
+			ctx.fillText(lines[mid],xText,yText);
+			for (var line=mid+1; line<linesNo; line++) {
+				var y = yText  + (line-mid)*fontSize ;					
+				ctx.fillText(lines[line],xText,y);
+			}	
+		} else {
+			for (var line=0; line<mid; line++) {
+				var y = yText - fontSize/2-(mid-line-1)*fontSize  ;					
+				ctx.fillText(lines[line],xText,y);
+			}	
+			for (var line=mid; line<linesNo; line++) {
+				var y = yText  + fontSize/2 + (line-mid)*fontSize ;					
+				ctx.fillText(lines[line],xText,y);
+			}	
+		}				
+		
+	} else {
+		// single line of text
+		ctx.fillText(text,xText,yText);
+	}
+	
+	//reset shadow
+	ctx.shadowBlur = 0;
+	ctx.shadowOffsetX = 0; 
+	ctx.shadowOffsetY = 0; 
+			
+	// animate using jQuery
+	var x = left+radius;
+	var y = canHeight/2;
+	var r = radius;
+	var from = 1;
+	var to = r-2*d;		
+	var grd = ctx.createLinearGradient(left, canHeight/2-radius/2*Math.sin(3*Math.PI/8), left+2*radius, canHeight/2+radius*Math.sin(3*Math.PI/8));
+	grd.addColorStop(0, "white");    
+	grd.addColorStop(1, color); 
+    $({ n: from }).animate({ n: to}, {
+       duration: 1000,    
+       step: function(now, fx) {
+          drawCircle(id,x,y,now,d, grd);       
+       },
+       complete: function() {
+    	   
+       }
+    }); 
+    
+    function drawCircle(id,x,y,r,d,grd) {
+    	
+    	var can = document.getElementById(id);
+    	var ctx = can.getContext('2d');  
+    	
+    	// clear circle (a little bigger)
+    	ctx.beginPath();
+    	ctx.arc(x,y,r+2,2* Math.PI , 0, false);
+    	ctx.closePath();
+    	ctx.fillStyle="white";
+    	ctx.fill();
+    	
+    	// outer circle
+    	ctx.strokeStyle = "gray";  
+    	ctx.beginPath();
+    	ctx.arc(x,y,r,2* Math.PI , 0, false);
+    	ctx.closePath();    	
+    	ctx.stroke();     	
+    	
+    	// inner circle
+    	if (r > 2*d) {
+	    	ctx.beginPath();	
+	    	ctx.arc(x,y,r-2*d,2* Math.PI , 0, false);
+	    	ctx.closePath();
+	    	ctx.stroke(); 			
+	    	ctx.fillStyle = grd;
+	    	ctx.fill();
+    	}
+    }
+			
+}
+
+
 function nextWidget(type, myjson, id, zoom) {				
 				
 	if (type == "indicator") {
 		indicator(id, myjson, zoom);
 	} else if (type == "display") {
 		display(id, myjson, zoom);
+	} else if (type == "alarm") {
+		alarm(id, myjson, zoom);
 	}
 }	
 
